a55354d59926160f1c44e9b334c1ed578a2cb203
Fred7877 <darkfred78@gmail.com>
2020-12-12 10:56:42 +0100

    fix bulma

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,23 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            fancybox: '@fancyapps/fancybox',
+            'owl.carousel': 'owl.carousel',
+            sweetalert2: 'sweetalert2',
+            ziggy: path.resolve('vendor/tightenco/ziggy/dist')
+        }
+    }
+});
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .js('resources/js/show-game.js', 'public/js')
+    .js('resources/js/comments.js', 'public/js')
+    .js('resources/js/nav-bar.js', 'public/js')
+    .js('resources/js/ziggy.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .autoload({
+        jquery: ['$', 'jQuery', 'window.jQuery']
+    })
+    .sourceMaps();
