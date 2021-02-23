348f1cfbc5361d1740ccad6a3d8bd80c2ebc41c3
Fred7877 <darkfred78@gmail.com>
2021-02-23 15:55:13 +0100

    add api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,29 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/games', function (Request $request) {
+    $platformSlug = $request->get('platform');
+
+    return Cache::remember('api_all_games_'.$platformSlug, 3600, function () use($platformSlug){
+         $query = Game::with(['screenshots', 'cover', 'platforms', 'genres'])
+            ->where('first_release_date', '<', Carbon::now());
+            if ($platformSlug !== null) {
+                $query->where('platforms.slug', $platformSlug);
+            }
+
+        return $query->orderBy('first_release_date', 'desc')->get()->toArray();
+    });
+});
+
+Route::get('/platforms', function (Request $request) {
+   return Cache::remember('api_all_platforms', 3600, function () {
+        return Platform::all()->toArray();
+    });
+});
+
+Route::get('/genres', function (Request $request) {
+    return Cache::remember('api_all_genres', 3600, function () {
+        return Genre::all()->toArray();
+    });
+});
