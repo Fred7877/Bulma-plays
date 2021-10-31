aabf18e5ccf0f4771fdc0a89263d9a3d92ba11bc
Fred7877 <darkfred78@gmail.com>
2021-10-31 19:37:17 +0100

    add bloc pubs

--- resources/views/frontend/partials/card-game.blade.php
+++ resources/views/frontend/partials/card-game.blade.php
@@ -9,16 +9,3 @@
         @include('frontend.partials.card-game-desktop')
     </div>
 </div>
-
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3267490048749348"
-        crossorigin="anonymous"></script>
-<!-- Bloc1 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3267490048749348"
-     data-ad-slot="6345150642"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
